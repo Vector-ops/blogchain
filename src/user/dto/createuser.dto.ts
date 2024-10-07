dacfcbe549253b3577dec164d89f2d78976b5606
Vector-ops <sumith2827@gmail.com>
2024-10-07 22:52:20 +0530

    refactor: add max length to fields

--- src/user/dto/createuser.dto.ts
+++ src/user/dto/createuser.dto.ts
@@ -24,11 +24,14 @@ export class CreateUserDto {
 
   @IsEmail()
   @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(96)
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
     message:
       'Minimum eight characters, at least one letter, one number and one special character',
