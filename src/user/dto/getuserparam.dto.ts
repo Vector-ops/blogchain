03fda1c832fcdd7ef037a4a3ec56c438d6c222ac
Vector-ops <sumith2827@gmail.com>
2024-10-07 22:52:47 +0530

    docs: add decorators for swagger docs

--- src/user/dto/getuserparam.dto.ts
+++ src/user/dto/getuserparam.dto.ts
@@ -1,7 +1,12 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsOptional } from 'class-validator';
 
 export class GetUserParamDto {
+  @ApiPropertyOptional({
+    description: 'Get user with id',
+    example: 123,
+  })
   @IsOptional()
   @IsInt()
   @Type(() => Number)
