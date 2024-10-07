210da8d94f913315eeed10cf4be68ab6f035f836
Vector-ops <sumith2827@gmail.com>
2024-10-07 22:51:56 +0530

    feat: add post entity

---
+++ src/post/post.entity.ts
@@ -0,0 +1,70 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { MetaOptionsDto } from './dto/metaoptions.dto';
+import { PostStatus } from './enums/poststatus.enum';
+import { PostType } from './enums/posttype.enum';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 512,
+    nullable: false,
+  })
+  title: string;
+
+  @Column({
+    type: 'enum',
+    enum: PostType,
+    nullable: false,
+    default: PostType.POST,
+  })
+  type: PostType;
+
+  @Column({
+    type: 'varchar',
+    length: 256,
+    nullable: false,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  content?: string;
+
+  @Column({
+    type: 'enum',
+    enum: PostStatus,
+    nullable: false,
+    default: PostStatus.DRAFT,
+  })
+  status: PostStatus;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  schema?: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 1024,
+  })
+  featuredImageUrl?: string;
+
+  @Column({
+    type: 'timestamp',
+    nullable: true,
+  })
+  publishOn?: Date;
+
+  tags?: string[];
+
+  metaOptions?: MetaOptionsDto;
+}
