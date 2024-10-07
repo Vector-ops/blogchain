e9ad028e077218d83897f2cd6b1d2f4f15c4b66b
Vector-ops <sumith2827@gmail.com>
2024-10-07 22:54:41 +0530

    refactor: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,26 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger config
+
+  const config = new DocumentBuilder()
+    .setTitle('BlogChain Documentation')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService(
+      "You are lucky there's no terms of services for this api",
+    )
+    .setLicense(
+      'MIT License',
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt',
+    )
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
