08fe4afae8e8aa60910fbe36ea19329a08e76e6d
Vector-ops <sumith2827@gmail.com>
2024-10-07 22:51:48 +0530

    refactor: add basic controller

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dto/createpost.dto';
+import { UpdatePostDto } from './dto/updatepost.dto';
+import { UpdatePostParamDto } from './dto/updatepostparam.dto';
 import { PostService } from './providers/post.service';
 
 @Controller('post')
+@ApiTags('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @ApiOperation({
+    summary: 'Creates a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 if your post is created successfully',
+  })
+  @Post()
+  public createPost(@Body() createPost: CreatePostDto) {
+    return 'Created post';
+  }
+
+  @ApiOperation({
+    summary: 'Update blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'You get a 200 if your post is updated successfully',
+  })
+  @Patch('/:id')
+  public updatePost(
+    @Param() updatePostParam: UpdatePostParamDto,
+    @Body() updatePostDto: UpdatePostDto,
+  ) {
+    return 'patched';
+  }
 }
