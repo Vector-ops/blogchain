c8092b4327b338b3cff0e3313b3299c1e81b29b6
Vector-ops <sumith2827@gmail.com>
2024-10-07 22:54:25 +0530

    refactor: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // Import Modules
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from 'src/user/user.module';
-import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [
+    UserModule,
+    PostModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [User],
+        synchronize: true,
+        port: 5432,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        host: 'localhost',
+        database: process.env.POSTGRES_DB,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
