d7357bde97d447398baa9629032664904ece2766
Vector-ops <sumith2827@gmail.com>
2024-10-11 19:22:28 +0530

    feat: add meta option controller

---
+++ src/metaoption/metaoption.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { MetaOptionsDto } from './dto/createmetaoptions.dto';
+import { MetaoptionService } from './metaoption.service';
+
+@Controller('metaoption')
+export class MetaoptionController {
+  constructor(private readonly metaoptionService: MetaoptionService) {}
+
+  @Post()
+  public create(@Body() createMetaOptionsDto: MetaOptionsDto) {
+    return this.metaoptionService.create(createMetaOptionsDto);
+  }
+}
