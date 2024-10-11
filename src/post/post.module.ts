614f225892d23fe6171e4f39a8cbc5dfc7de88a1
Vector-ops <sumith2827@gmail.com>
2024-10-11 19:25:10 +0530

    refactor: import modules

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MetaOption } from 'src/metaoption/metaoption.entity';
+import { TagsModule } from 'src/tags/tags.module';
+import { UserModule } from 'src/user/user.module';
 import { PostController } from './post.controller';
+import { Post } from './post.entity';
 import { PostService } from './providers/post.service';
 
 @Module({
   controllers: [PostController],
   providers: [PostService],
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([Post, MetaOption]),
+    TagsModule,
+  ],
 })
 export class PostModule {}
