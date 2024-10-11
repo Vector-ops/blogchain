764dfea022b593733be36dde8d2b005dd9434496
Vector-ops <sumith2827@gmail.com>
2024-10-11 19:22:13 +0530

    feat: add meta option service

---
+++ src/metaoption/providers/metaoption.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MetaOptionsDto } from './dto/createmetaoptions.dto';
+import { MetaOption } from './metaoption.entity';
+
+@Injectable()
+export class MetaoptionService {
+  constructor(
+    @InjectRepository(MetaOption)
+    private readonly metaoptionRepository: Repository<MetaOption>,
+  ) {}
+  public async create(createMetaoptionDto: MetaOptionsDto) {
+    let metaoption = this.metaoptionRepository.create(createMetaoptionDto);
+
+    return await this.metaoptionRepository.save(metaoption);
+  }
+}
