2c0eae0f08d10ebbbf6fc4c3a04415f6a2b5167e
Vector-ops <sumith2827@gmail.com>
2024-10-14 19:53:18 +0530

    chore: add provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserService } from './providers/user.service';
+import { UserCreateManyProvider } from './providers/usercreatemany.provider';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserCreateManyProvider],
   exports: [UserService],
   imports: [TypeOrmModule.forFeature([User])],
 })
