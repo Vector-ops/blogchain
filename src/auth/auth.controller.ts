9c214635e1d77940fb4c855535979215c98e5ff6
Vector-ops <sumith2827@gmail.com>
2024-10-26 17:02:16 +0530

    feat: implement singin controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Auth } from './decorators/auth.decorator';
+import { SignInDto } from './dto/signin.dto';
+import { AuthType } from './enums/auth-type.enum';
 import { AuthService } from './providers/auth.service';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @Auth(AuthType.None)
+  public async signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
 }
