e09b9597393678d801eddf38100aaba5bd2d6bb3
Vector-ops <sumith2827@gmail.com>
2024-10-26 17:01:00 +0530

    feat: add auth service

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -1,7 +1,24 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  RequestTimeoutException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from 'src/user/providers/user.service';
+import { SignInDto } from '../dto/signin.dto';
+import { SigninProvider } from './signin.provider';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(
+    @Inject(forwardRef(() => UserService))
+    private readonly userService: UserService,
+
+    private readonly signInProvider: SigninProvider,
+  ) {}
+
+  public async signIn(signInDto: SignInDto) {
+    return await this.signInProvider.signIn(signInDto);
+  }
 }
