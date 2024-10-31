a6f264359dc6414449a3ab6f53d2cfbccc88c358
Vector-ops <sumith2827@gmail.com>
2024-10-31 16:21:07 +0530

    chore: add aws and smtp config

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,4 +3,12 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('appConfig', () => ({
   environment: process.env.NODE_ENV || 'development',
   apiVersion: process.env.API_VERSION,
+  awsBucketName: process.env.AWS_PUBLIC_BUCKET_NAME,
+  awsRegion: process.env.AWS_PUBLIC_BUCKET_NAME,
+  awsCloudfrontUrl: process.env.AWS_PUBLIC_BUCKET_NAME,
+  awsAccessKey: process.env.AWS_PUBLIC_BUCKET_NAME,
+  awsSecretAccessKey: process.env.AWS_PUBLIC_BUCKET_NAME,
+  mailHost: process.env.MAIL_HOST,
+  smtpUsername: process.env.SMTP_USERNAME,
+  smtpPassword: process.env.SMTP_PASSWORD,
 }));
